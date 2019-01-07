7848fb56be747fc27315d0d134f6cc83db6b8790
wendellrodrigues <wendell.rodrigues@sjsu.edu>
2019-01-06 20:51:54 -0800

    Post model completed

--- models/Post.js
+++ models/Post.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 //Create Schema
-
 const PostSchema = new Schema({
     user: {
         type: Schema.Types.ObjectId,
@@ -17,6 +16,42 @@ const PostSchema = new Schema({
     },
     avatar: {
         type: String
+    },
+    likes: [
+        {
+            user: {
+                type: Schema.Types.ObjectId,
+                ref: 'users'
+            }
+        }
+    ],
+    comments: [
+        {
+            user: {
+                type: Schema.Types.ObjectId,
+                ref: 'users'
+            },
+            text: {
+                type: String,
+                required: true
+            },
+            name: {
+                type: String
+            },
+            avatar: {
+                type: String
+            },
+            date: {
+                type: Date,
+                default: Date.now
+            }
+        },
+    ],
+    date: {
+        type: Date,
+        default: Date.now
     }
 
-})
+});
+
+module.exports = Post = mongoose.model('post', PostSchema);
